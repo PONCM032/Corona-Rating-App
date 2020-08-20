9bb63334504570f01d769ee9441c93ef44ba79f2
Melanie <melanie-ponce@hotmail.com>
2020-08-19 22:42:51 -0400

    add login/register - basic styling in react

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="uk-navbar-container">
+      <a className="uk-active" href="/">
+        Corona Rating App
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
--- client/src/pages/index/index.js
+++ client/src/pages/index/index.js
@@ -1,5 +1,18 @@
 import React from "react";
+import CreateBtn from "../../components/CreateBtn/index";
+import Nav from "../../components/Nav/index";
 
+function Index(){
+    return(
+        <div className="bkgc">
+        <Nav/>
+        <h1>HELLO REACT</h1>
+        <CreateBtn/>
+        </div>
+    )
+}
+
+export default Index;
 //login/singup
 
 //passport
