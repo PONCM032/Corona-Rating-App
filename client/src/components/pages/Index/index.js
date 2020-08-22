bd93f2eff735edb2e657041de297bdefb63885f7
Demetrio <demetrioalmeida@gmail.com>
2020-08-22 09:14:00 -0700

    GoogleMap component

--- client/src/components/pages/Index/index.js
+++ client/src/components/pages/Index/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import CreateBtn from "../../CreateBtn/index";
 import Nav from "../../Nav/index";
+import WrappedMap from "../../GoogleMap";
 
-function Index(props){
-    return(
-        <div className="bkgc">
-        <Nav/>
-        <h1>{`hello ${props.user.username}`}</h1>
-        <h3>{props.authorized ? 'logged in' : 'logged out'}</h3>
-        
-        <CreateBtn/>
-        </div>
-    )
+function Index(props) {
+  return (
+    <div className="bkgc">
+      <Nav />
+      <h1>{`hello ${props.user.username}`}</h1>
+      <h3>{props.authorized ? "logged in" : "logged out"}</h3>
+
+      <CreateBtn />
+
+      <div style={{ margin: "0 auto", width: "50vw", height: "50vh" }}>
+      {props.authorized ? <WrappedMap
+          isMarkerShown
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: "100%" }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        /> : "logged out"}
+      </div>
+    </div>
+  );
 }
 
 export default Index;
