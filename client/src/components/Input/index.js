ad5ba586dec1f1bd310cc1b3e7290312ec7558d8
Melanie <melanie-ponce@hotmail.com>
2020-08-24 21:02:49 -0400

    navbar styling, update header that shows logged in status, add input field

---
+++ client/src/components/Input/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Input(){
+    return(
+        <form>
+        <div class="uk-margin" uk-margin>
+        <input class="uk-input uk-form-width-large" type="text" placeholder="Search"/>
+        <div class="uk-button uk-button-default">Submit</div>
+    </div>
+    </form>
+    )
+}
+
+export default Input;
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,34 @@
 import React from "react";
+import "./style.css"
 
 function Nav() {
   return (
-    <nav className="uk-navbar-container">
-      <a className="uk-active" href="/">
-        Corona Rating App
-      </a>
-    </nav>
+    <nav class="uk-navbar-container uk-margin" uk-navbar="boundary-align: true; align: center;">
+    <div class="uk-navbar-left">
+
+        <a class="uk-navbar-item uk-logo" href="#">Corona Rating App</a>
+
+        <ul class="uk-navbar-nav">
+            <li>
+                <a href="#">
+                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+                    Features
+                </a>
+            </li>
+        </ul>
+
+        <div class="uk-navbar-item">
+            <div>Some <a href="#">Link</a></div>
+        </div>
+        
+        <div class="uk-navbar-item button">
+            <form action="javascript:void(0)">
+                <button class="uk-button uk-button-default">Button</button>
+            </form>
+        </div>
+
+    </div>
+</nav>
   );
 }
 
