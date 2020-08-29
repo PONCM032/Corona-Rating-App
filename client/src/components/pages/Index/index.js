0bd42ab0c30a5851d93f72892b6ce354cf9b06fe
Demetrio <demetrioalmeida@gmail.com>
2020-08-29 11:25:04 -0400

    Search_bar

--- client/src/components/pages/Index/index.js
+++ client/src/components/pages/Index/index.js
@@ -1,35 +1,41 @@
-import React, {} from "react";
+import React from "react";
 import CreateBtn from "../../CreateBtn/index";
 import LoginBtn from "../../LoginBtn/index";
 import LogoutBtn from "../../LogoutBtn/index";
 import Nav from "../../Nav/index";
-import Input from "../../Input/index"
+import Input from "../../Input/index";
 import WrappedMap from "../../GoogleMap";
+import GeoLocation from "../../GeoLocation/";
+import SearchField from "../../SearchField";
+import SearchBox from "react-google-maps/lib/components/places/SearchBox";
 
-function Index(props){
-    return(
-        <div className="bkgc">
-        <Nav/>
-        <h1>{props.authorized ? `Welcome ${props.user.username}`: "Welcome!"}</h1>
+function Index(props) {
+  return (
+    <div className="bkgc">
+      <Nav />
+      <h1>
+        {props.authorized ? `Welcome ${props.user.username}` : "Welcome!"}
+      </h1>
 
-        <Input/>
+      {/* <Input /> */}
+      <SearchField />
 
-        <h3>{props.authorized ? <LogoutBtn/> : <LoginBtn/>}</h3>
+      <h3>{props.authorized ? <LogoutBtn /> : <LoginBtn />}</h3>
 
-        <div style={{ margin: "0 auto", width: "50vw", height: "50vh" }}>
-          {props.authorized ? <WrappedMap
-              isMarkerShown
-              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
-              loadingElement={<div style={{ height: `100%` }} />}
-              containerElement={<div style={{ height: "100%" }} />}
-              mapElement={<div style={{ height: `100%` }} />}
-            /> : "logged out"}
-         </div>
+      <div style={{ margin: "0 auto", width: "50vw", height: "50vh" }}>
+        <GeoLocation />
+        {/* <WrappedMap
+          isMarkerShown
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: "100%" }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        /> */}
+      </div>
 
-        <CreateBtn/>
-
-        </div>
-    )
+      <CreateBtn />
+    </div>
+  );
 }
 
 export default Index;
