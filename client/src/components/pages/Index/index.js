5003cd93ce46614eeac7b1761fd9a0f9725f010c
Melanie <melanie-ponce@hotmail.com>
2020-08-29 12:20:47 -0400

    "commit"

--- client/src/components/pages/Index/index.js
+++ client/src/components/pages/Index/index.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import React, {} from "react";
-=======
 import React from "react";
 import CreateBtn from "../../CreateBtn/index";
 // import LoginBtn from "../../LoginBtn/index";
 // import LogoutBtn from "../../LogoutBtn/index";
->>>>>>> ce459ba54fd7cd0bd0b220c942c662fa4cfc55d5
 import Nav from "../../Nav/index";
 import Input from "../../Input/index";
 import WrappedMap from "../../GoogleMap";
@@ -39,24 +35,9 @@ function Index(props) {
         /> */}
       </div>
 
-<<<<<<< HEAD
-        <div style={{ margin: "0 auto", width: "50vw", height: "50vh" }}>
-          {props.authorized ? <WrappedMap
-              isMarkerShown
-              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
-              loadingElement={<div style={{ height: `100%` }} />}
-              containerElement={<div style={{ height: "100%" }} />}
-              mapElement={<div style={{ height: `100%` }} />}
-            /> : "logged out"}
-         </div>
-
-        </div>
-    )
-=======
       <CreateBtn />
     </div>
   );
->>>>>>> ce459ba54fd7cd0bd0b220c942c662fa4cfc55d5
 }
 
 export default Index;
